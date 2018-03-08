61214c95faff9505d6cd2caf920814eb539c5251
logan-markewich <logan.markewich@live.com>
2018-03-08 11:02:47 -0600

    added semaphare/threads

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+import threading
+
+# this shows how to create a semaphore and a thread
+fileSem = threading.Semaphore()
+getRssi_t = threading.Thread(target=getRssi)
+
+def getRssi():
+  fileSem.acquire()
+  #insert code here
+  fileSem.release()
+
 def main():
   # code here
 
