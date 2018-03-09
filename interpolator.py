55374b334a7ca97b81da3663c764c660cba956d1
Lane Larochelle <lanelarochelle@gmail.com>
2018-03-09 15:06:14 -0500

    Create interpolator.py
    RSSI reading have been doctored for the sake of this but this is just the beginning

---
+++ interpolator.py
@@ -0,0 +1,18 @@
+#Lane Larochelle
+#March 8th 2018 
+
+# x is in meters
+# y is corresonding rssi
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+from plotly.tools import FigureFactory as FF
+
+import numpy as np
+import pandas as pd
+import scipy
+
+##FAIR WARNING THESE ARE A LITTLE TAPERED AS TO CREATE A GRADIENT 
+known_points = np.array([(0, -24), (.25, -42.67), (.5, -63.667), (.75, -69), (1,-79), (1.25, -82), (1.5, -79.33), (1.75, -85.33), (2, -86.33), (2.25, -88.33), (2.5,-90), (2.75,-90.33), (3,-91)])
+meters = points[:,0]
+rssis = points[:,1]
